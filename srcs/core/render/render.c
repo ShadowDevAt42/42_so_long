3ddfdfeac91a6a34b6f312a8f585da0cfdd99acf
Shadow <ShadowDevAt42@pm.me>
2025-02-09 00:05:52 +0100

    Refactor de la gestion des portails : suppression de la fonction de rendu conditionnel et appel direct à la fonction de rendu de sortie

--- srcs/core/render/render.c
+++ srcs/core/render/render.c
@@ -6,33 +6,11 @@
 /*   By: fdi-tria <fdi-tria@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 08:32:49 by fdi-tria          #+#    #+#             */
-/*   Updated: 2025/02/08 22:22:21 by fdi-tria         ###   ########.fr       */
+/*   Updated: 2025/02/09 00:04:05 by fdi-tria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
-
-static void	render_portal_if_needed(t_game *game)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < game->map->height)
-	{
-		j = 0;
-		while (j < game->map->width)
-		{
-			if (game->map->grid[i][j] == 'E')
-			{
-				render_exit(game);
-				return ;
-			}
-			j++;
-		}
-		i++;
-	}
-}
 
 static void	handle_portal_animation(t_game *game, int *counter)
 {
@@ -45,7 +23,7 @@ static void	handle_portal_animation(t_game *game, int *counter)
 	{
 		*counter = 0;
 		update_portal_anim(game);
-		render_portal_if_needed(game);
+		render_exit(game);
 	}
 }
 
@@ -81,7 +59,7 @@ void	render_game(t_game *game)
 	render_walls(game);
 	render_corner(game);
 	render_collectibles(game);
-	render_portal_if_needed(game);
+	render_exit(game);
 	render_player(game);
 	render_ui(game);
 	render_collectibles_ui(game);
